f751e3ad4219b2dbde341650a9fbe5fef0f60b8b
eyuell21 <abrahameyuel4@gmail.com>
2025-08-14 14:48:02 +0100

    Set up backend server to serve random Office quotes.

---
+++ backend/server.mjs
@@ -0,0 +1,61 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors())
+
+// The Office (U.S.) quotes
+const quotes = [
+  {
+    quote: "I'm not superstitious, but I am a little stitious.",
+    author: "Michael Scott"
+  },
+  {
+    quote: "Bears. Beets. Battlestar Galactica.",
+    author: "Jim Halpert"
+  },
+  {
+    quote: "I DECLARE BANKRUPTCY!",
+    author: "Michael Scott"
+  },
+  {
+    quote: "Sometimes I'll start a sentence and I don't even know where it's going. I just hope I find it along the way.",
+    author: "Michael Scott"
+  },
+  {
+    quote: "I'm running away from my responsibilities. And it feels good.",
+    author: "Michael Scott"
+  },
+  {
+    quote: "Identity theft is not a joke, Jim! Millions of families suffer every year.",
+    author: "Dwight Schrute"
+  },
+  {
+    quote: "I talk a lot, so I've learned to just tune myself out.",
+    author: "Kelly Kapoor"
+  },
+  {
+    quote: "Through concentration, I can raise and lower my cholesterol at will.",
+    author: "Dwight Schrute"
+  },
+  {
+    quote: "I feel God in this Chili’s tonight.",
+    author: "Pam Beesly"
+  },
+  {
+    quote: "I am Beyoncé, always.",
+    author: "Michael Scott"
+  }
+];
+
+app.get('/quote', (res, req) => {
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    res.json(randomQuote);
+})
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`The Office Quote API running at http://localhost:${PORT}`);
+});
